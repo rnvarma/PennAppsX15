112421b25982fbb41a10c68a9397e8fd7b08f78d
Rohan Varma <rnvarma@andrew.cmu.edu>
2015-01-16 23:22:44 -0500

    framework 7 integrated

--- PennAppsX15/platforms/ios/www/js/index.js
+++ PennAppsX15/platforms/ios/www/js/index.js
@@ -19,8 +19,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        // this.bindEvents();
-        window.location.href = "templates/homePage.html"
+        this.bindEvents();
     },
     // Bind Event Listeners
     //
@@ -34,18 +33,17 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        window.location.href = "/templates/homePage.html"
-        // app.receivedEvent('deviceready');
+        app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    // receivedEvent: function(id) {
-    //     var parentElement = document.getElementById(id);
-    //     var listeningElement = parentElement.querySelector('.listening');
-    //     var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-    //     listeningElement.setAttribute('style', 'display:none;');
-    //     receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-    //     console.log('Received Event: ' + id);
-    // }
+        console.log('Received Event: ' + id);
+    }
 };
